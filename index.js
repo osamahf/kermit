27bd7dc5cf16dede17752327191232d8bc4bd07a
osamahf <oussama.inpt@gmail.com>
2015-03-19 12:20:58 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var http = require('http');
+//loads http module
+var app=http.createServer(function (req, res) {
+//creates server
+res.writeHead(200, {'Content-Type': 'text/plain'});
+//sets the right header and status code
+res.end('Hello World\n');
+//outputs string with line end symbol
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
+
+
